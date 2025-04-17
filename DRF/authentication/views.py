28998bb04e23d8a1ea8e4ceed671986c896c6376
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-04-17 20:27:21 +0100

    Started jwt auth

--- DRF/authentication/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from rest_framework import views, status, generics
-from rest_framework.response import Response
-
-class SignUpView(generics.GenericAPIView):
-    
